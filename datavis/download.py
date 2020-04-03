3a9e1976ba7b354c2a67eda7127d86fc5118c878
tracek <lukasz.tracewski@outlook.com>
2020-04-03 08:55:16 +0200

    add resume download and dry run

--- datavis/download.py
+++ datavis/download.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import glob
 import click
 import requests
 import logging
@@ -55,21 +56,42 @@ def download(url, filename):
                     fh.write(chunk)
 
 
+def remove_files_already_downloaded(output_dir: str, output_path_with_link: list) -> list:
+    files_to_download = [output_path for link, output_path in output_path_with_link]
+    downloaded_files_path = glob.glob(os.path.join(output_dir, '*'))
+    downloaded_files = [os.path.basename(s) for s in downloaded_files_path]
+    remaining_files = set(files_to_download) - set(downloaded_files)
+    output_path_with_link_d = {k: v for v, k in output_path_with_link}
+    remaining_files_with_links = [(output_path_with_link_d[filename], filename) for filename in remaining_files]
+    return list(remaining_files_with_links)
+
+
 @cli.command('file', help='Helper CLI to download files. Currently works with a rather special structure defined by shell script provided by Topher')
 @click.option("--input", "-in", type=click.Path(exists=True), required=True, help="Path to a directory with audio in WAV format.")
 @click.option("--output", "-out", type=click.Path(exists=False, file_okay=False, writable=True), required=True, help="Output directory.")
 @click.option("--jobs", "-j", type=click.INT, default=8, help="Number of threads to run.", show_default=True)
-def file_download(input, output, jobs):
+@click.option('--fresh', default=False, is_flag=True, help='Ignore any files that might have been already downloaded and start fresh', show_default=True)
+@click.option('--dry', default=False, is_flag=True, help='Dry run. Do not download anythyng, just show how much will be downloaded', show_default=True)
+def file_download(input, output, jobs, fresh, dry):
     with open(input, 'r') as fin:
-        files_to_download = fin.read().splitlines()
+        output_path_with_link = fin.read().splitlines()
 
-    if files_to_download[0].startswith('#'):
-        files_to_download.pop(0)
-    files_to_download = [clean(s) for s in files_to_download]
+    if output_path_with_link[0].startswith('#'):
+        output_path_with_link.pop(0)
+    output_path_with_link = [clean(s) for s in output_path_with_link]
 
-    _ = Parallel(n_jobs=jobs, backend='threading')(
-        delayed(url_to_file)(url=url, filename=os.path.join(output, filename))
-        for url, filename in tqdm(files_to_download))
+    total_files = len(output_path_with_link)
+    remaining_files_no = total_files
+    if not fresh:
+        output_path_with_link = remove_files_already_downloaded(output_dir=output, output_path_with_link=output_path_with_link)
+        remaining_files_no = len(output_path_with_link)
+
+    if dry:
+        print(f'Number of files to download: {remaining_files_no} out of total {total_files}')
+    else:
+        _ = Parallel(n_jobs=jobs, backend='threading')(
+            delayed(url_to_file)(url=url, filename=os.path.join(output, filename))
+            for url, filename in tqdm(output_path_with_link))
 
 
 @cli.command('resume', help='Resume failed downloads')
